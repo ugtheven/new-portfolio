398cdaacb5c15cfe1eb95711598f23448f177547
S-i-d-V <me@ugotheveny.dev>
2024-01-11 23:55:57 +0100

    ✨ Navbar is fixed to the top

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -9,6 +9,8 @@ import { FaGithubSquare } from "react-icons/fa";
 function Home() {
   return (
     <div className="container">
+      
+      {/* Premiere section */}
       <div className="landing">
         <img src={logo} alt="Logo" className="logo" />
         <h1 className="title">@ugtheven</h1>
@@ -24,10 +26,20 @@ function Home() {
         </div>
 
         <img src={arrowDown} alt="arrow down" className="more" />
+
+        <img src={waveMobile} alt="wave" className="wave-mobile" />
+        <img src={waveDesktop} alt="wave" className="wave-desktop" />
       </div>
 
-      <img src={waveMobile} alt="wave" className="wave-mobile" />
-      <img src={waveDesktop} alt="wave" className="wave-desktop" />
+      {/* Deuxieme section */}
+      <div className="about-me">
+        <h3 className="section-title">A propos de moi</h3>
+        <p className="section-text">
+          Je suis un développeur front-end React junior basé à Paris, France.
+          J'ai une passion pour le développement web et la création d'interfaces
+          utilisateur qui apportent de la valeur aux utilisateurs.
+        </p>
+      </div>
     </div>
   );
 }
