c0673156ef6fb34f555c4aeec0850866e27fcbdd
S-i-d-V <me@ugotheveny.dev>
2024-01-13 23:59:25 +0100

    ✨ Footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,11 +1,50 @@
+import { FaGithubSquare, FaLinkedin, FaPhoneAlt } from "react-icons/fa";
 import "./Footer.scss";
 
-import logo from "../../assets/logo.png";
 const Footer = () => {
   return (
     <footer className="footer">
-        <img src={logo} alt="" />
-        <span>© 2024 conçu et mis en ligne par <a href="https://github.com/ugtheven">@ugtheven</a></span>
+      {/* Socials links */}
+      <div className="socials">
+        <span>MES RESEAUX</span>
+        <div className="links">
+          <a
+            href="https://www.linkedin.com/in/ugo-theveny/"
+            className="social-link"
+          >
+            <FaLinkedin className="social-icon" />
+            LinkedIn
+          </a>
+          -
+          <a href="https://github.com/ugtheven" className="social-link">
+            <FaGithubSquare className="social-icon" />
+            Github
+          </a>
+        </div>
+      </div>
+
+      {/* Contact */}
+      <div className="contact">
+        <span>COMMENT ME JOINDRE ?</span>
+        <div className="infos">
+          <div>
+            <FaPhoneAlt className="social-icon" />
+            <span>ugotheveny@gmail.com</span>
+          </div>
+          -
+          <div>
+            <span>06 34 44 49 37</span>
+          </div>
+        </div>
+      </div>
+
+      {/* "Copyright" */}
+      <div className="endbar">
+        <p>
+          © 2024 conçu et mis en ligne par{" "}
+          <a href="https://github.com/ugtheven">@ugtheven</a>
+        </p>
+      </div>
     </footer>
   );
 };
