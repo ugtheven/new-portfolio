24a4626f1f0fa474bacccdcdba2913d51678145c
S-i-d-V <me@ugotheveny.dev>
2024-01-14 23:59:53 +0100

    ✨ Enhance form styles

--- src/Components/ContactForm/ContactForm.tsx
+++ src/Components/ContactForm/ContactForm.tsx
@@ -1,35 +1,45 @@
 import { ValidationError, useForm } from "@formspree/react";
+import "./ContactForm.scss";
+import { BsFillSendCheckFill, BsFillSendFill } from "react-icons/bs";
 
 const ContactForm = () => {
   const [state, handleSubmit] = useForm("mbjnlkyp");
-
 
   return (
     <div className="form">
       <span className="title">FORMULAIRE DE CONTACT</span>
       {state.succeeded ? (
-        <p className="success-message">
-          Le formulaire a été envoyé avec succès !
-        </p>
+        <div>
+          <BsFillSendCheckFill />
+          <p className="success-message">
+            Le formulaire a été envoyé avec succès !
+          </p>
+        </div>
       ) : (
         <form onSubmit={handleSubmit}>
-          <label htmlFor="email">Nom</label>
+          <label htmlFor="email">Votre nom / Nom de votre entreprise</label>
           <input id="name" type="name" name="name" />
           <ValidationError prefix="Name" field="name" errors={state.errors} />
 
-          <label htmlFor="email">Email Address</label>
+          <label htmlFor="email">Votre addresse mail</label>
           <input id="email" type="email" name="email" />
           <ValidationError prefix="Email" field="email" errors={state.errors} />
 
-          <textarea id="message" name="message" />
+          <label htmlFor="message">Votre message</label>
+          <textarea id="message" name="message" rows={4} />
           <ValidationError
             prefix="Message"
             field="message"
             errors={state.errors}
           />
 
-          <button type="submit" disabled={state.submitting}>
-            Submit
+          <button
+            type="submit"
+            className="submit-button"
+            disabled={state.submitting}
+          >
+            <BsFillSendFill className="submit-icon" />
+            <span>Envoyer</span>
           </button>
         </form>
       )}
--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,47 +1,47 @@
-import { FaGithubSquare, FaLinkedin, FaPhoneAlt } from "react-icons/fa";
+import { FaGithubSquare, FaLinkedin, FaPhoneAlt, FaMailBulk } from "react-icons/fa";
 import "./Footer.scss";
 import ContactForm from "../ContactForm/ContactForm";
 
 const Footer = () => {
-
   return (
     <footer className="footer">
-      {/* Socials links */}
-      <div className="socials">
-        <span className="title">MES RESEAUX</span>
-        <div className="links">
-          <a
-            href="https://www.linkedin.com/in/ugo-theveny/"
-            className="social-link"
-          >
-            <FaLinkedin className="social-icon" />
-            LinkedIn
-          </a>
-          -
-          <a href="https://github.com/ugtheven" className="social-link">
-            <FaGithubSquare className="social-icon" />
-            Github
-          </a>
-        </div>
-      </div>
-
-      {/* Contact */}
-      <div className="contact">
-        <span className="title">COMMENT ME JOINDRE ?</span>
-        <div className="infos">
-          <div>
-            <FaPhoneAlt className="social-icon" />
-            <span>ugotheveny@gmail.com</span>
-          </div>
-          -
-          <div>
-            <span>06 34 44 49 37</span>
+      <div className="content">
+        {/* Socials links */}
+        <div className="socials">
+          <span className="title">MES RESEAUX</span>
+          <div className="links">
+            <a
+              href="https://www.linkedin.com/in/ugo-theveny/"
+              className="social-link"
+            >
+              <FaLinkedin className="social-icon" />
+              LinkedIn
+            </a>
+            -
+            <a href="https://github.com/ugtheven" className="social-link">
+              <FaGithubSquare className="social-icon" />
+              Github
+            </a>
           </div>
         </div>
-      </div>
+        {/* Contact infos */}
+        <div className="contact">
+          <span className="title">COMMENT ME JOINDRE ?</span>
+          <div className="infos">
+            <div>
+              <FaMailBulk className="social-icon" />
+              <span>ugotheveny@gmail.com</span>
+            </div>
+            <div>
+              <FaPhoneAlt className="social-icon" />
+              <span>06 34 44 49 37</span>
+            </div>
+          </div>
+        </div>
 
-      {/* Form */}
-      <ContactForm />
+        {/* Form */}
+        <ContactForm />
+      </div>
 
       {/* "Copyright" */}
       <div className="endbar">
