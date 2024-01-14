5f15c6be44bdb40beb6a8382237162c953f766d7
S-i-d-V <me@ugotheveny.dev>
2024-01-14 23:15:04 +0100

    📦️ Add and setup formspree

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,7 +1,10 @@
 import { FaGithubSquare, FaLinkedin, FaPhoneAlt } from "react-icons/fa";
 import "./Footer.scss";
+import { ValidationError, useForm } from "@formspree/react";
 
 const Footer = () => {
+  const [state, handleSubmit] = useForm("mbjnlkyp");
+
   return (
     <footer className="footer">
       {/* Socials links */}
@@ -35,6 +38,37 @@ const Footer = () => {
           <div>
             <span>06 34 44 49 37</span>
           </div>
+        </div>
+
+        <div className="form">
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="email">Nom</label>
+            <input id="name" type="name" name="name" />
+            <ValidationError
+              prefix="Name"
+              field="name"
+              errors={state.errors}
+            />
+
+            <label htmlFor="email">Email Address</label>
+            <input id="email" type="email" name="email" />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+
+            <textarea id="message" name="message" />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
+
+            <button type="submit" disabled={state.submitting}>
+              Submit
+            </button>
+          </form>
         </div>
       </div>
 
