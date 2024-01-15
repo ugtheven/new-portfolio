59c80119bbbb3832e9e97b41c602d26cccad16ea
S-i-d-V <me@ugotheveny.dev>
2024-01-15 23:11:50 +0100

    🔥 Delete contact route + able Projects page

--- src/main.tsx
+++ src/main.tsx
@@ -8,10 +8,9 @@ import {
 import "./index.scss";
 import App from "./App";
 import Home from "./Pages/Home/Home";
-// import Projects from "./Pages/Projects/Projects";
+import Projects from "./Pages/Projects/Projects";
 // import Resume from "./Pages/Resume/Resume";
 // import Skills from "./Pages/Skills/Skills";
-// import Contact from "./Pages/Contact/Contact";
 import Waiting from "./Pages/Waiting/Waiting";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -22,8 +21,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           <Route path="/" element={<Home />} />
           <Route path="/resume" element={<Waiting />} />
           <Route path="/skills" element={<Waiting />} />
-          <Route path="/projects" element={<Waiting />} />
-          <Route path="/contact" element={<Waiting />} />
+          <Route path="/projects" element={<Projects />} />
         </Routes>
       </App>
     </Router>
