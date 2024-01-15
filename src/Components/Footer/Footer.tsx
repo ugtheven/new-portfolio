cfd9bedb816c960116699c36703949c77b5cb181
S-i-d-V <me@ugotheveny.dev>
2024-01-15 22:49:35 +0100

    ✨ Footer is responsive

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,4 +1,9 @@
-import { FaGithubSquare, FaLinkedin, FaPhoneAlt, FaMailBulk } from "react-icons/fa";
+import {
+  FaGithubSquare,
+  FaLinkedin,
+  FaPhoneAlt,
+  FaMailBulk,
+} from "react-icons/fa";
 import "./Footer.scss";
 import ContactForm from "../ContactForm/ContactForm";
 
@@ -6,35 +11,37 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="content">
-        {/* Socials links */}
-        <div className="socials">
-          <span className="title">MES RESEAUX</span>
-          <div className="links">
-            <a
-              href="https://www.linkedin.com/in/ugo-theveny/"
-              className="social-link"
-            >
-              <FaLinkedin className="social-icon" />
-              LinkedIn
-            </a>
-            -
-            <a href="https://github.com/ugtheven" className="social-link">
-              <FaGithubSquare className="social-icon" />
-              Github
-            </a>
-          </div>
-        </div>
-        {/* Contact infos */}
-        <div className="contact">
-          <span className="title">COMMENT ME JOINDRE ?</span>
-          <div className="infos">
-            <div>
-              <FaMailBulk className="social-icon" />
-              <span>ugotheveny@gmail.com</span>
+        <div className="leftside">
+          {/* Socials links */}
+          <div className="socials">
+            <span className="title">MES RESEAUX</span>
+            <div className="links">
+              <a
+                href="https://www.linkedin.com/in/ugo-theveny/"
+                className="social-link"
+              >
+                <FaLinkedin className="social-icon" />
+                LinkedIn
+              </a>
+              -
+              <a href="https://github.com/ugtheven" className="social-link">
+                <FaGithubSquare className="social-icon" />
+                Github
+              </a>
             </div>
-            <div>
-              <FaPhoneAlt className="social-icon" />
-              <span>06 34 44 49 37</span>
+          </div>
+          {/* Contact infos */}
+          <div className="contact">
+            <span className="title">COMMENT ME JOINDRE ?</span>
+            <div className="infos">
+              <div className="info">
+                <FaMailBulk className="social-icon" />
+                <span>ugotheveny@gmail.com</span>
+              </div>
+              <div className="info">
+                <FaPhoneAlt className="social-icon" />
+                <span>06 34 44 49 37</span>
+              </div>
             </div>
           </div>
         </div>
