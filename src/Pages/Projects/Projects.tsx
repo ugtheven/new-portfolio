1bf9e0822524b9b1a8777c86e0d774c4025eec3b
S-i-d-V <me@ugotheveny.dev>
2024-01-15 23:26:19 +0100

    🚧 Setting up Projects page

--- src/Pages/Projects/Projects.tsx
+++ src/Pages/Projects/Projects.tsx
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react";
 import "./Projects.scss";
 
+interface Project {
+  title: string;
+  description: string;
+  image: string;
+  link: string;
+}
+
 function Projects() {
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    import("./projects.json").then((data) => {
+      setProjects(data.default);
+    });
+  }, []);
+
   return (
     <div className="projects-container">
-      <div className="projects">Projets</div>
+      <div className="projects">
+        {projects.map((project, index) => (
+          <div className="project" key={index}>
+            <div className="project-image">
+              <img src={project.image} alt={project.title} />
+            </div>
+            <div className="project-title">{project.title}</div>
+            <div className="project-description">{project.description}</div>
+            <div className="project-link">
+              <a href={project.link} target="_blank" rel="noreferrer">
+                Voir le projet
+              </a>
+            </div>
+          </div>
+        ))}
+        </div>
     </div>
   );
 }
