4118721a49ad145364b3d6cb83c759162080bc3c
S-i-d-V <me@ugotheveny.dev>
2024-01-16 22:06:14 +0100

    ✨ Real data + structure of list project

--- src/Pages/Projects/Projects.tsx
+++ src/Pages/Projects/Projects.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 import "./Projects.scss";
+import { FaExternalLinkAlt, FaGithubSquare } from "react-icons/fa";
 
 interface Project {
   title: string;
   description: string;
+  type: string;
   image: string;
   link: string;
+  repository: string;
 }
 
 function Projects() {
@@ -20,21 +23,46 @@ function Projects() {
   return (
     <div className="projects-container">
       <div className="projects">
+        {/* Project map */}
         {projects.map((project, index) => (
-          <div className="project" key={index}>
-            <div className="project-image">
-              <img src={project.image} alt={project.title} />
-            </div>
-            <div className="project-title">{project.title}</div>
-            <div className="project-description">{project.description}</div>
-            <div className="project-link">
-              <a href={project.link} target="_blank" rel="noreferrer">
-                Voir le projet
+          <div className="project-card" key={index}>
+            <img
+              src={project.image}
+              className="project-image"
+              alt={project.title}
+            />
+
+            <span>{project.type}</span>
+
+            <span className="project-title">{project.title}</span>
+
+            <p className="project-description">{project.description}</p>
+
+            <div className="project-links">
+              {project.link && (
+                <a
+                  href={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="project-link"
+                >
+                  <FaExternalLinkAlt className="project-icon" />
+                  <span>View Website</span>
+                </a>
+              )}
+              <a
+                href={project.repository}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project-link"
+              >
+                <FaGithubSquare className="project-icon" />
+                <span>View Repository</span>
               </a>
             </div>
           </div>
         ))}
-        </div>
+      </div>
     </div>
   );
 }
